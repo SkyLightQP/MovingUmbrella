4997ce7f9b61165af60728909d842a87a8db1907
DaegyeomHa <combbm@naver.com>
2021-10-23 20:04:22 +0900

    feat(android): 커스텀 툴바 구현

--- android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
@@ -1,11 +1,18 @@
 package me.daegyeo.movingumbrella
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerToolbar()
+    }
+
+    private fun registerToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.new_toolbar)
+        setSupportActionBar(toolbar)
     }
 }
