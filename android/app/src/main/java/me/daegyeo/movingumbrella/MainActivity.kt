06dc151e71a13fa52965b3fedb0d366f67187a05
DaegyeomHa <combbm@naver.com>
2021-10-23 22:48:39 +0900

    feat(android): Kakao Map 적용

--- android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
@@ -1,18 +1,39 @@
 package me.daegyeo.movingumbrella
 
+import android.Manifest
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import net.daum.mf.map.api.MapView
+
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSIONS_REQUEST_READ_LOCATION = 0x00000001
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.INTERNET,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_READ_LOCATION);
         registerToolbar()
+        registerKakaoMap()
     }
 
     private fun registerToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.new_toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    private fun registerKakaoMap() {
+        val mapView = MapView(this)
+        val mapViewContainer = findViewById<View>(R.id.map) as ViewGroup
+        mapViewContainer.addView(mapView)
     }
 }
