fff1869df36663cab2006900ffae8de7c2c535f8
DaegyeomHa <combbm@naver.com>
2021-10-24 23:38:06 +0900

    fix(android): 디버그 로그 삭제

--- android/app/src/main/java/me/daegyeo/movingumbrella/BluetoothReceiver.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/BluetoothReceiver.kt
@@ -4,12 +4,12 @@ import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.widget.Toast
 
 
 class BluetoothReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val device: BluetoothDevice? = intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-        Log.e("disconnect", device!!.name)
+        Toast.makeText(context, "${device!!.name} 연결이 끊겼습니다.", Toast.LENGTH_LONG).show()
     }
 }
