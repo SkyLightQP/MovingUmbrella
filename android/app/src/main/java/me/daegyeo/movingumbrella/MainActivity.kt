95188c24ba58a8de08c09f91ecaaced8608cef2f
DaegyeomHa <combbm@naver.com>
2021-10-25 21:08:17 +0900

    feat(android): 블루투스 연동

--- android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
@@ -1,9 +1,13 @@
 package me.daegyeo.movingumbrella
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.naver.maps.map.LocationTrackingMode
@@ -13,11 +17,17 @@ import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
 import me.daegyeo.movingumbrella.data.MapData
 import me.daegyeo.movingumbrella.runtimePermission.Permission
+import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val TAG = "MovingUmbrella"
     private val ACCESS_LOCATION_CODE = 1000
+    private val BLUETOOTH_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
+
     private lateinit var locationSource: FusedLocationSource
     private lateinit var mMapView: MapView
+    private lateinit var bluetoothAdapter: BluetoothAdapter
 
     companion object {
         var mapData: MapData = MapData(null, Pair(0.0, 0.0))
@@ -36,9 +46,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMapView = findViewById(R.id.map)
         locationSource = FusedLocationSource(this, ACCESS_LOCATION_CODE)
         mMapView.getMapAsync(this)
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
         if (!fineLocation.isGrant() || !coarseLocation.isGrant()) {
-            Toast.makeText(this, "위치 추적을 위해 위치 권한을 허용해주세요.", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "위치 추적을 위해 우산 권한을 허용해주세요.", Toast.LENGTH_LONG).show()
             Permission.requestPermissions(
                 this,
                 arrayOf(
@@ -47,6 +58,35 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 ),
                 ACCESS_LOCATION_CODE
             )
+        }
+
+        findViewById<ImageButton>(R.id.bluetooth).setOnClickListener {
+            if (!bluetoothAdapter.isEnabled) return@setOnClickListener
+
+            val pairedDevices = bluetoothAdapter.bondedDevices
+            if (pairedDevices.size > 0) {
+                AlertDialog.Builder(this).apply {
+                    setTitle("우산 연결하기")
+                    val deviceNames = pairedDevices.map {
+                        it.name
+                    }
+                    setItems(deviceNames.toTypedArray()) { _, which ->
+                        connectBluetoothDevice(deviceNames[which])
+                    }
+                    create().show()
+                }
+            }
+        }
+    }
+
+    private fun connectBluetoothDevice(deviceName: String) {
+        try {
+            val device = bluetoothAdapter.bondedDevices.filter { it.name == deviceName }
+            val socket = device[0].createRfcommSocketToServiceRecord(BLUETOOTH_UUID)
+            socket.connect()
+        } catch (ex: IOException) {
+            Log.e(TAG, ex.stackTraceToString())
+            Toast.makeText(this, "${deviceName}에 연결을 할 수 없습니다.\n다시 시도해주세요.", Toast.LENGTH_SHORT).show()
         }
     }
 
--- android/app/src/main/java/me/daegyeo/movingumbrella/receivers/BluetoothReceiver.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/receivers/BluetoothReceiver.kt
@@ -11,18 +11,20 @@ import me.daegyeo.movingumbrella.MainActivity
 
 class BluetoothReceiver : BroadcastReceiver() {
     val mapData = MainActivity.mapData
+    val DEVICE_NAME = "HC-06"
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val device: BluetoothDevice? = intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
         val name = device!!.name
 
-        if (name == "Umbrella") {
+        if (name == DEVICE_NAME) {
             Marker().apply {
                 position = LatLng(
                     mapData.lastLocation.first,
                     mapData.lastLocation.second
                 )
                 map = mapData.naverMap
+                captionText = "최근 우산 위치"
             }
         }
     }
