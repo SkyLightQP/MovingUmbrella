972a3235d36a812d8c428308a1022f229bd61b63
DaegyeomHa <combbm@naver.com>
2021-10-25 15:48:48 +0900

    feat(android): add BluetoothConnectReceiver

---
+++ android/app/src/main/java/me/daegyeo/movingumbrella/receivers/BluetoothConnectReceiver.kt
@@ -0,0 +1,11 @@
+package me.daegyeo.movingumbrella.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BluetoothConnectReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
