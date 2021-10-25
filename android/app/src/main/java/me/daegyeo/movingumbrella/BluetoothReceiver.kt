34630d9b1da999b792238f48796798c80f094923
DaegyeomHa <combbm@naver.com>
2021-10-25 14:37:57 +0900

    feat(android): 블루투스 연결이 끊겼을 때 마커 추가

--- android/app/src/main/java/me/daegyeo/movingumbrella/BluetoothReceiver.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/BluetoothReceiver.kt
@@ -5,11 +5,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.overlay.Marker
 
 
 class BluetoothReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val device: BluetoothDevice? = intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+        val name = device!!.name
         Toast.makeText(context, "${device!!.name} 연결이 끊겼습니다.", Toast.LENGTH_LONG).show()
+        if (name == "Umbrella") {
+            Toast.makeText(context, "${MainActivity.lastLocation.first}", Toast.LENGTH_LONG).show()
+
+            Marker().apply {
+                position = LatLng(MainActivity.lastLocation.first, MainActivity.lastLocation.second)
+                map = MainActivity.mNaverMap
+            }
+        }
     }
 }
