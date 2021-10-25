26c49a5824993d555b625194b91e79aed7584a3e
DaegyeomHa <combbm@naver.com>
2021-10-25 14:16:04 +0900

    fix(android): 펄미션 요청 로직 개선

--- android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
@@ -10,8 +10,8 @@ import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
-import me.daegyeo.movingumbrella.runtimePermission.Permission
 import com.naver.maps.map.util.FusedLocationSource
+import me.daegyeo.movingumbrella.runtimePermission.Permission
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val ACCESS_LOCATION_CODE = 1000
@@ -19,22 +19,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mNaverMap: NaverMap
     private lateinit var mMapView: MapView
 
+    private val fineLocation = Permission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+    private val coarseLocation = Permission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        registerToolbar()
+        val toolbar = findViewById<Toolbar>(R.id.new_toolbar)
+        setSupportActionBar(toolbar)
+
         mMapView = findViewById(R.id.map)
         locationSource = FusedLocationSource(this, ACCESS_LOCATION_CODE)
         mMapView.getMapAsync(this)
 
-        val fineLocation =
-            Permission(this, Manifest.permission.ACCESS_FINE_LOCATION).checkPermission()
-        val coarseLocation =
-            Permission(this, Manifest.permission.ACCESS_COARSE_LOCATION).checkPermission()
-        if (fineLocation && coarseLocation) {
-            tracking()
-        } else {
+        if (!fineLocation.isGrant() || !coarseLocation.isGrant()) {
             Toast.makeText(this, "위치 추적을 위해 위치 권한을 허용해주세요.", Toast.LENGTH_LONG).show()
             Permission.requestPermissions(
                 this,
@@ -47,18 +46,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    private fun tracking() {
+        mNaverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
     override fun onMapReady(naverMap: NaverMap) {
         mNaverMap = naverMap
         naverMap.locationSource = locationSource
-    }
-
-    private fun registerToolbar() {
-        val toolbar = findViewById<Toolbar>(R.id.new_toolbar)
-        setSupportActionBar(toolbar)
-    }
-
-    private fun tracking() {
-        mNaverMap.locationTrackingMode = LocationTrackingMode.Follow
+        if (fineLocation.isGrant() && coarseLocation.isGrant()) tracking()
     }
 
     override fun onRequestPermissionsResult(
