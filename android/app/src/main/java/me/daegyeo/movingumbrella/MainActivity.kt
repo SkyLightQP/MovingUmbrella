56e18ee68e99ab60a87918c52d99158af89731c5
DaegyeomHa <combbm@naver.com>
2021-10-25 13:22:28 +0900

    feat(android): add tracking code

--- android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
+++ android/app/src/main/java/me/daegyeo/movingumbrella/MainActivity.kt
@@ -6,24 +6,34 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import me.daegyeo.movingumbrella.runtimePermission.Permission
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val ACCESS_LOCATION_CODE = 1000
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var mNaverMap: NaverMap
+    private lateinit var mMapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         registerToolbar()
-
+        mMapView = findViewById(R.id.map)
+        locationSource = FusedLocationSource(this, ACCESS_LOCATION_CODE)
+        mMapView.getMapAsync(this)
 
         val fineLocation =
             Permission(this, Manifest.permission.ACCESS_FINE_LOCATION).checkPermission()
         val coarseLocation =
             Permission(this, Manifest.permission.ACCESS_COARSE_LOCATION).checkPermission()
         if (fineLocation && coarseLocation) {
-            // TODO: Tracking
+            tracking()
         } else {
             Toast.makeText(this, "위치 추적을 위해 위치 권한을 허용해주세요.", Toast.LENGTH_LONG).show()
             Permission.requestPermissions(
@@ -37,9 +47,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onMapReady(naverMap: NaverMap) {
+        mNaverMap = naverMap
+        naverMap.locationSource = locationSource
+    }
+
     private fun registerToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.new_toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    private fun tracking() {
+        mNaverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 
     override fun onRequestPermissionsResult(
@@ -51,7 +70,7 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             ACCESS_LOCATION_CODE -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Tracking
+                    tracking()
                 }
             }
         }
